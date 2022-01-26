48f4aae7387dca035d8af529c0fdcf6386fe0d12
Keith Sharp <kms@passback.co.uk>
2022-01-26 18:54:41 +0000

    Implemented message functionality.

--- rsmqctl.py
+++ rsmqctl.py
@@ -1,3 +1,4 @@
+from http import client
 from tabnanny import verbose
 import click
 import json
@@ -79,6 +80,106 @@ def delete(ctx, name):
         sys.exit(0)
     else:
         print('Failed to delete queue: {}'.format(name))
+        sys.exit(1)
+
+@cli.group()
+@click.pass_context
+def message(ctx):
+    pass
+
+@message.command()
+@click.option('-n', '--name', 'name', required=True)
+@click.option('-m', '--message', 'message', required=True)
+@click.option('-d', '--delay', 'delay', default=0)
+@click.pass_context
+def send(ctx, name, message, delay):
+    queue = ctx.obj.getQueueAttributes(qname=name, quiet=True).execute()
+    if not queue:
+        print('No such queue: {}'.format(name))
+        sys.exit(1)
+
+    result = ctx.obj.sendMessage(qname=name, message=message, delay=delay, quiet=True).execute()
+    if result:
+        print(result)
+        sys.exit(0)
+    else:
+        print('Failed to send message: {}'.format(message))
+        sys.exit(1)
+
+@message.command()
+@click.option('-n', '--name', 'name', required=True)
+@click.option('-i', '--id', 'id', required=True)
+@click.pass_context
+def delete(ctx, name, id):
+    queue = ctx.obj.getQueueAttributes(qname=name, quiet=True).execute()
+    if not queue:
+        print('No such queue: {}'.format(name))
+        sys.exit(1)
+    
+    result = ctx.obj.deleteMessage(qname=name, id=id, quiet=True).execute()
+    if result:
+        sys.exit(0)
+    else:
+        print('Failed to delete message ID: {}'.format(id))
+        sys.exit(1)
+
+@message.command()
+@click.option('-n', '--name', 'name', required=True)
+@click.option('-t', '--vt', 'vt')
+@click.pass_context
+def receive(ctx, name, vt):
+    queue = ctx.obj.getQueueAttributes(qname=name, quiet=True).execute()
+    if not queue:
+        print('No such queue: {}'.format(name))
+        sys.exit(1)
+    
+    client = ctx.obj.receiveMessage(qname=name, quiet=True)
+    if vt:
+        client.vt(vt)
+    message = client.execute()
+    if message:
+        message['id'] = message['id'].decode('UTF-8')
+        message['message'] = message['message'].decode('UTF-8')
+        print(json.dumps(message))
+    else:
+        print('No messages on queue: {}'.format(name))
+    sys.exit(0)
+
+@message.command()
+@click.option('-n', '--name', 'name', required=True)
+@click.pass_context
+def pop(ctx, name):
+    queue = ctx.obj.getQueueAttributes(qname=name, quiet=True).execute()
+    if not queue:
+        print('No such queue: {}'.format(name))
+        sys.exit(1)
+
+    message = ctx.obj.popMessage(qname=name, quite=True).execute()
+    if message:
+        message['id'] = message['id'].decode('UTF-8')
+        message['message'] = message['message'].decode('UTF-8')
+        print(json.dumps(message))
+    else:
+        print('No messages on queue: {}'.format(name))
+    sys.exit(0)
+
+@message.command()
+@click.option('-n', '--name', 'name', required=True)
+@click.option('-i', '--id', 'id', required=True)
+@click.option('-t', '--vt', 'vt', required=True)
+@click.pass_context
+def visibility(ctx, name, id, vt):
+    queue = ctx.obj.getQueueAttributes(qname=name, quiet=True).execute()
+    if not queue:
+        print('No such queue: {}'.format(name))
+        sys.exit(1)
+    
+    result = ctx.obj.changeMessageVisibility(qname=name, id=id, vt=vt).execute()
+    if result:
+        print('Message ID: {} visibility timeout set to {}s'.format(id, vt))
+        sys.exit(0)
+    else:
+        print('Failed to change visibility timeout for message ID: {}'.format(id))
         sys.exit(1)
 
 if __name__ == '__main__':
